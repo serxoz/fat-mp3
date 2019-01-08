a1ff8ba56037fdd3a3aa74aff149da8bbd9d1e9a
Richard Denton <isdampe@gmail.com>
2019-01-09 09:51:12 +1100

    FIX: Show play-fs with on end event

--- examples/play-fs.js
+++ examples/play-fs.js
@@ -1,14 +1,8 @@
 const fmp3 = require("../index.js");
 
 const player = new fmp3("test.mp3");
+player.on("end", () => {
+	console.log("Playback finished.");
+});
 player.play();
 
-setTimeout(()=>{
-	console.log("Pause");
-	player.pause();
-}, 10000);
-
-setTimeout(()=>{
-	console.log("Play");
-	player.play();
-}, 20000);
