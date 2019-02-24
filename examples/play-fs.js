11ed7fe55eeb7ce0255be4cd1228cc5434335075
Richard Denton <isdampe@gmail.com>
2019-02-24 20:01:36 +1100

    ENHANCE: Add volume level set ability

--- examples/play-fs.js
+++ examples/play-fs.js
@@ -1,6 +1,11 @@
 const fmp3 = require("../index.js");
 
 const player = new fmp3("test.mp3");
+
+setTimeout(() => {
+	player.setVolume(0.5);
+}, 2000);
+
 player.on("end", () => {
 	console.log("Playback finished.");
 });
